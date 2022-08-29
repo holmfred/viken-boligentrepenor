c89f3b8bceb5bb59c843027890e86b5ea069590b
Holmfred <fredrik1304@live.no>
2022-08-30 00:45:42 +0200

    Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,21 @@
-import Heading from "../layout/Heading";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
  return (
-   <div className="container">
-   <Heading content="Footer" />
+   <footer>
+   <div className="godkjent">
+    <img src={"../../images/boligmappa.png"} alt="Boligmappa"/>
+    <img src={"../../images/sentralt_godkjent.jpg"} alt="Sentralt Godkjent"/>
    </div>
+   <div>
+    <h3>Kontakt Oss:</h3>
+        <p>463 16 571</p>
+        <p>post@vikenbe.as</p>
+        <div className="socials">
+            <Link to="https://www.facebook.com/VikenBoligentreprenor" class="fab fa-facebook"/>
+            <Link to="https://www.instagram.com/vikenboligentreprenor/" class="fab fa-instagram"/>
+        </div>
+   </div>
+   </footer>
  );
 }
