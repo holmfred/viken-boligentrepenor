bceb00967903ee47bd3d3c3dfb286ae181ee5a6c
Holmfred <fredrik1304@live.no>
2022-08-30 00:56:55 +0200

    Nav Done & Footer missing links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,10 +7,10 @@ export default function Footer() {
     <img src={"../../images/boligmappa.png"} alt="Boligmappa"/>
     <img src={"../../images/sentralt_godkjent.jpg"} alt="Sentralt Godkjent"/>
    </div>
-   <div>
-    <h3>Kontakt Oss:</h3>
-        <p>463 16 571</p>
-        <p>post@vikenbe.as</p>
+   <div className="all-socials">
+    <h5>Kontakt Oss:</h5>
+        <p><i class="fas fa-phone"></i>463 16 571</p>
+        <p><i class="fas fa-envelope"></i>post@vikenbe.as</p>
         <div className="socials">
             <Link to="https://www.facebook.com/VikenBoligentreprenor" class="fab fa-facebook"/>
             <Link to="https://www.instagram.com/vikenboligentreprenor/" class="fab fa-instagram"/>
