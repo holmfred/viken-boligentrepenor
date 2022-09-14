9c62dee968578b4946820f527b30dfb773b531f1
Holmfred <fredrik1304@live.no>
2022-09-14 22:54:26 +0200

    Carousel Update

--- src/components/home/HomeCarousel.js
+++ src/components/home/HomeCarousel.js
@@ -7,37 +7,35 @@ function HomeCarousel() {
         <img
           className="d-block w-100"
           src="../../images/terrasse.jpg"
-          alt="Natre"
+          alt="Terrasse"
         />
         <Carousel.Caption>
-          <h3>Natre</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Terrasse</h3>
+          <p>Tidligere Oppdrag</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
           src="../../images/terrasse.jpg"
-          alt="Second slide"
+          alt="Terrasse"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Terrasse</h3>
+          <p>Tidligere Oppdrag</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
           src="../../images/terrasse.jpg"
-          alt="Third slide"
+          alt="Terrasse"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3>Terrasse</h3>
+          <p>Tidligere Oppdrag</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
